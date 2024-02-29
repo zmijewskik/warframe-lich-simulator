a2a7b7a4ca5183c98653415aafafa51b22abbb23
zmijewskik <kzmijewski@protonmail.com>
2024-02-29 23:47:59 +0100

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+document.getElementById('btn-generate-lich')?.addEventListener('click', generateImages);
 
-import confetti from 'canvas-confetti';
+function shuffleArray(array: number[]): number[] {
+  const shuffledArray = array.slice();
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+function generateImages() {
+  const images = document.querySelectorAll('.image');
+
+  // Generate an array of unique numbers from 1 to 8
+  const uniqueNumbers = Array.from({ length: 8 }, (_, index) => index + 1);
+  
+  // Shuffle the array and pick the first three
+  const shuffledNumbers = shuffleArray(uniqueNumbers).slice(0, 3);
+
+  // Update the images with the shuffled numbers
+  images.forEach((image, index) => {
+    image.textContent = shuffledNumbers[index].toString();
+  });
+}
