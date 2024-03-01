b6b63ba0c6356781c90edcc366f053542925988b
zmijewskik <kzmijewski@protonmail.com>
2024-03-01 18:52:53 +0100

    Add: sign icons / number->icon implementation

--- docs/dist/index.js
+++ docs/dist/index.js
@@ -1,5 +1,27 @@
 document.getElementById("btn-generate-lich")?.addEventListener("click", generateImages);
 document.getElementById("btn-check-combination")?.addEventListener("click", checkCombination);
+const allSignNames = [
+  "Fass",
+  "Jahu",
+  "Khra",
+  "Lohk",
+  "Netra",
+  "Ris",
+  "Vome",
+  "Xata"
+];
+function populateDropdownOptions(selectId, allSignNames2) {
+  const selectElement = document.getElementById(selectId);
+  allSignNames2.forEach((imageName, index) => {
+    const option = document.createElement("option");
+    option.value = imageName;
+    option.text = `${imageName}`;
+    selectElement.add(option);
+  });
+}
+populateDropdownOptions("number1", allSignNames);
+populateDropdownOptions("number2", allSignNames);
+populateDropdownOptions("number3", allSignNames);
 function shuffleArray(array) {
   const shuffledArray = array.slice();
   for (let i = shuffledArray.length - 1; i > 0; i--) {
@@ -10,38 +32,51 @@ function shuffleArray(array) {
 }
 function generateImages() {
   const images = document.querySelectorAll(".image");
-  const uniqueNumbers = Array.from({length: 8}, (_, index) => index + 1);
-  const shuffledNumbers = shuffleArray(uniqueNumbers).slice(0, 3);
+  const signNames = [
+    "Fass",
+    "Jahu",
+    "Khra",
+    "Lohk",
+    "Netra",
+    "Ris",
+    "Vome",
+    "Xata"
+  ];
+  const shuffledSignNames = shuffleArray(signNames).slice(0, 3);
   images.forEach((image, index) => {
-    image.textContent = shuffledNumbers[index].toString();
+    const imageElement = image;
+    const signNameElement = document.getElementById(`signName${index + 1}`);
+    imageElement.style.backgroundImage = `url('img/requiem-icons/${shuffledSignNames[index] + "RequiemIcon.webp"}')`;
+    signNameElement.textContent = shuffledSignNames[index];
   });
 }
 function checkCombination() {
-  const userNumbers = getUserNumbers();
-  const generatedNumbers = getGeneratedNumbers();
-  if (arraysEqual(userNumbers, generatedNumbers)) {
-    alert("Congratulations! Your combination matches the generated one!");
+  const userSignNames = getUserSignNames();
+  const generatedSignNames = getGeneratedSignNames();
+  if (arraysEqual(userSignNames, generatedSignNames)) {
+    alert("Congratulations! Your combination matches!");
   } else {
-    alert("Oops! Your combination does not match the generated one. Try again!");
+    alert("Oops! Your combination does not match. Try again!");
   }
 }
 function resetUserInputFields() {
   const userInputForm = document.getElementById("user-combination-form");
   userInputForm.reset();
 }
-function getUserNumbers() {
-  const number1 = parseInt(document.getElementById("number1").value, 10);
-  const number2 = parseInt(document.getElementById("number2").value, 10);
-  const number3 = parseInt(document.getElementById("number3").value, 10);
-  return [number1, number2, number3];
+function getUserSignNames() {
+  const signName1 = document.getElementById("number1").value;
+  const signName2 = document.getElementById("number2").value;
+  const signName3 = document.getElementById("number3").value;
+  return [signName1, signName2, signName3];
 }
-function getGeneratedNumbers() {
+function getGeneratedSignNames() {
   const images = document.querySelectorAll(".image");
-  const generatedNumbers = [];
+  const generatedSignNames = [];
   images.forEach((image) => {
-    generatedNumbers.push(parseInt(image.textContent || "0", 10));
+    const signNameElement = document.getElementById(`signName${generatedSignNames.length + 1}`);
+    generatedSignNames.push(signNameElement.textContent || "");
   });
-  return generatedNumbers;
+  return generatedSignNames;
 }
 function arraysEqual(arr1, arr2) {
   return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,34 @@
 document.getElementById('btn-generate-lich')?.addEventListener('click', generateImages);
 document.getElementById('btn-check-combination')?.addEventListener('click', checkCombination);
 
-function shuffleArray(array: number[]): number[] {
+const allSignNames: string[] = [
+  'Fass',
+  'Jahu',
+  'Khra',
+  'Lohk',
+  'Netra',
+  'Ris',
+  'Vome',
+  'Xata',
+  // 'Oull', 
+]
+
+function populateDropdownOptions(selectId: string, allSignNames: string[]): void {
+  const selectElement = document.getElementById(selectId) as HTMLSelectElement;
+
+  allSignNames.forEach((imageName, index) => {
+    const option = document.createElement('option');
+    option.value = imageName;
+    option.text = `${imageName}`;
+    selectElement.add(option);
+  });
+}
+
+populateDropdownOptions('number1', allSignNames);
+populateDropdownOptions('number2', allSignNames);
+populateDropdownOptions('number3', allSignNames);
+
+function shuffleArray(array: string[]): string[] {
   const shuffledArray = array.slice();
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -13,26 +40,36 @@ function shuffleArray(array: number[]): number[] {
 function generateImages() {
   const images = document.querySelectorAll('.image');
 
-  // Generate an array of unique numbers from 1 to 8
-  const uniqueNumbers = Array.from({ length: 8 }, (_, index) => index + 1);
-  
-  // Shuffle the array and pick the first three
-  const shuffledNumbers = shuffleArray(uniqueNumbers).slice(0, 3);
+  const signNames: string[] = [
+    'Fass',
+    'Jahu',
+    'Khra',
+    'Lohk',
+    'Netra',
+    'Ris',
+    'Vome',
+    'Xata',
+  ]
 
-  // Update the images with the shuffled numbers
+  const shuffledSignNames: string[] = shuffleArray(signNames).slice(0, 3);
+
   images.forEach((image, index) => {
-    image.textContent = shuffledNumbers[index].toString();
+    const imageElement = image as HTMLElement;
+    const signNameElement = document.getElementById(`signName${index + 1}`) as HTMLElement;
+
+    imageElement.style.backgroundImage = `url('img/requiem-icons/${shuffledSignNames[index] + 'RequiemIcon.webp'}')`;
+    signNameElement.textContent = shuffledSignNames[index];
   });
 }
 
 function checkCombination() {
-  const userNumbers = getUserNumbers();
-  const generatedNumbers = getGeneratedNumbers();
+  const userSignNames = getUserSignNames();
+  const generatedSignNames = getGeneratedSignNames();
 
-  if(arraysEqual(userNumbers, generatedNumbers)){
-    alert("Congratulations! Your combination matches the generated one!");
+  if(arraysEqual(userSignNames, generatedSignNames)){
+    alert("Congratulations! Your combination matches!");
   } else {
-    alert("Oops! Your combination does not match the generated one. Try again!");
+    alert("Oops! Your combination does not match. Try again!");
   }
 }
 
@@ -41,25 +78,26 @@ function resetUserInputFields() {
   userInputForm.reset();
 }
 
-function getUserNumbers(): number[] {
-  const number1 = parseInt((document.getElementById('number1') as HTMLInputElement).value, 10);
-  const number2 = parseInt((document.getElementById('number2') as HTMLInputElement).value, 10);
-  const number3 = parseInt((document.getElementById('number3') as HTMLInputElement).value, 10);
+function getUserSignNames(): string[] {
+  const signName1 = (document.getElementById('number1') as HTMLInputElement).value;
+  const signName2 = (document.getElementById('number2') as HTMLInputElement).value;
+  const signName3 = (document.getElementById('number3') as HTMLInputElement).value;
 
-  return [number1, number2, number3];
+  return [signName1, signName2, signName3];
 }
 
-function getGeneratedNumbers(): number[] {
+function getGeneratedSignNames(): string[] {
   const images = document.querySelectorAll('.image');
-  const generatedNumbers: number[] = [];
+  const generatedSignNames: string[] = [];
 
   images.forEach((image) => {
-    generatedNumbers.push(parseInt(image.textContent || '0', 10));
+    const signNameElement = document.getElementById(`signName${generatedSignNames.length + 1}`) as HTMLElement;
+    generatedSignNames.push(signNameElement.textContent || '');
   });
 
-  return generatedNumbers;
+  return generatedSignNames;
 }
 
-function arraysEqual(arr1: number[], arr2: number[]): boolean {
+function arraysEqual(arr1: string[], arr2: string[]): boolean {
   return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
 }
